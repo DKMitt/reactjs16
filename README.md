a9f9d7e1b81484a4439a6f6bb0269b71656cfc74
Darryl Mittlestaedt <dkmitt@sbcglobal.net>
2018-01-12 19:39:31 -0600

    Working with Props and passing data from parent to child

--- README.md
+++ README.md
@@ -28,7 +28,9 @@ To take a look at some of the new features with React.JS 16 (aka React Fiber) an
 ----
 
 ### What I Learned From Working With This Tutorial
-- ​A Better Understanding, Creating & Working with Components
+- Installation and use of ​Create-React-App with an indepth understanding of the files and structure.
+- ​A better understanding of creating & working with Components.
+- ​Working with Props, using object assignment deconstructing, passing data from a parent to a child component.
 - ​State, Inline Conditionals & Basic Events
 - LifeCycle Methods 
 - ​
--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = "Welcome to React 16";
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome />
+          <Welcome text="Welcome to React 16" />
           <Subtitle />
         </header>
         <p className="App-intro">
@@ -24,8 +22,9 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
+    const { text } = this.props;
     return (
-      <h1 className="App-title">{welcome}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 }
@@ -41,8 +40,10 @@ class Subtitle extends Component {
 class Midcontent extends Component {
   render() {
     return (
-      <h3>These are Somethings</h3>
+      <h3>Here is Something Too!</h3>
     )
   }
 }
+
+
 export default App;
